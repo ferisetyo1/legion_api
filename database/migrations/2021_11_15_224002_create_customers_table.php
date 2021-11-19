fe387b6751cdcd937258bafcd22fb94a7c5c9948
ferisetyo1 <ferisetyo1@gmail.com>
2021-11-19 13:15:22 +0700

    update customer

--- database/migrations/2021_11_15_224002_create_customers_table.php
+++ database/migrations/2021_11_15_224002_create_customers_table.php
@@ -17,11 +17,11 @@ class CreateCustomersTable extends Migration
             $table->id('customer_id');
             $table->integer('customer_user_id');
             $table->string('customer_nama');
-            $table->integer('customer_tinggi');
-            $table->integer('customer_berat');
-            $table->dateTime('customer_tanggal_lahir');
-            $table->string('customer_gender');
-            $table->string('customer_image');
+            $table->integer('customer_tinggi')->default(null);
+            $table->integer('customer_berat')->default(null);
+            $table->dateTime('customer_tanggal_lahir')->default(null);
+            $table->string('customer_gender')->default(null);
+            $table->string('customer_image')->default(null);
             $table->dateTime("customer_create_at")->useCurrent();
             $table->dateTime("customer_update_at")->useCurrent()->useCurrentOnUpdate();
         });
