a654a9bf12912ed843973eb7aeee6b1d9adac46d
feri6 <feri@code.id>
2021-12-29 09:22:29 +0700

    harga trainer seeder

--- database/seeders/HargaTrainerSeeder.php
+++ database/seeders/HargaTrainerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\HargaTrainer;
 use Illuminate\Database\Seeder;
 
 class HargaTrainerSeeder extends Seeder
@@ -13,6 +14,25 @@ class HargaTrainerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        HargaTrainer::insert([
+            [
+                'ht_pt_id'=>1,
+                'ht_harga'=>160000,
+                'ht_waktu'=>60,
+                'ht_kht_id'=>1
+            ],
+            [
+                'ht_pt_id'=>1,
+                'ht_harga'=>180000,
+                'ht_waktu'=>60,
+                'ht_kht_id'=>2
+            ],
+            [
+                'ht_pt_id'=>1,
+                'ht_harga'=>180000,
+                'ht_waktu'=>60,
+                'ht_kht_id'=>3
+            ],
+        ]);
     }
 }
