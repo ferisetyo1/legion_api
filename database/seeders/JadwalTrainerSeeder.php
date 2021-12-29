11ba54ed2a8b2ff04a6fdda6333f1ce2ac4a1354
feri6 <feri@code.id>
2021-12-29 09:28:05 +0700

    jadwal trainer seeder

--- database/seeders/JadwalTrainerSeeder.php
+++ database/seeders/JadwalTrainerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\JadwalTrainer;
 use Illuminate\Database\Seeder;
 
 class JadwalTrainerSeeder extends Seeder
@@ -13,6 +14,79 @@ class JadwalTrainerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //jt_kategories input
+        $array = [
+            [
+                "kht_id" => 1,
+                "pt_confirm" => false,
+            ],
+            [
+                "kht_id" => 2,
+                "pt_confirm" => false,
+            ],
+            [
+                "kht_id" => 3,
+                "pt_confirm" => false,
+            ],
+        ];
+        $kategories = json_encode($array);
+        JadwalTrainer::insert([
+            [
+                'jt_pt_id'=>1,
+                'jt_gym_id'=>1,
+                'jt_pt_confirm'=>false,
+                'jt_gym_confirm'=>false,
+                'jt_hari'=>"senin",
+                'jt_kategories'=>$kategories,
+            ],
+            [
+                'jt_pt_id'=>1,
+                'jt_gym_id'=>1,
+                'jt_pt_confirm'=>false,
+                'jt_gym_confirm'=>false,
+                'jt_hari'=>"selasa",
+                'jt_kategories'=>$kategories,
+            ],
+            [
+                'jt_pt_id'=>1,
+                'jt_gym_id'=>1,
+                'jt_pt_confirm'=>false,
+                'jt_gym_confirm'=>false,
+                'jt_hari'=>"rabu",
+                'jt_kategories'=>$kategories,
+            ],
+            [
+                'jt_pt_id'=>1,
+                'jt_gym_id'=>1,
+                'jt_pt_confirm'=>false,
+                'jt_gym_confirm'=>false,
+                'jt_hari'=>"kamis",
+                'jt_kategories'=>$kategories,
+            ],
+            [
+                'jt_pt_id'=>1,
+                'jt_gym_id'=>1,
+                'jt_pt_confirm'=>false,
+                'jt_gym_confirm'=>false,
+                'jt_hari'=>"jum'at",
+                'jt_kategories'=>$kategories,
+            ],
+            [
+                'jt_pt_id'=>1,
+                'jt_gym_id'=>1,
+                'jt_pt_confirm'=>false,
+                'jt_gym_confirm'=>false,
+                'jt_hari'=>"sabtu",
+                'jt_kategories'=>$kategories,
+            ],
+            [
+                'jt_pt_id'=>1,
+                'jt_gym_id'=>1,
+                'jt_pt_confirm'=>false,
+                'jt_gym_confirm'=>false,
+                'jt_hari'=>"minggu",
+                'jt_kategories'=>$kategories,
+            ],
+        ]);
     }
 }
